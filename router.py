36be9f7d2e357f7fc98ae7c03214caa855dc3cf8
kimeisele <kim_eisele@icloud.com>
2025-11-19 16:33:00 +0100

    Remove root-level alias shims (cleanup)

--- router.py
+++
@@ -1,7 +0,0 @@
-"""Alias shim for AgentRouter (playbook router) to satisfy tests without sys.path hacks."""
-
-from importlib import import_module
-
-_mod = import_module("agency_os.00_system.playbook.router".replace("/", "."))
-AgentRouter = _mod.AgentRouter
-__all__ = ["AgentRouter"]
